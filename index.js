b3d681d8dfee52128133b19f7c275177032794ea
hazratgs <hazratgs@gmail.com>
2017-03-10 10:19:00 +0300

    Сохранение в файл

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 var webdriver = require('selenium-webdriver'),
     promise = webdriver.promise,
     By = webdriver.By,
@@ -24,37 +26,93 @@ setTimeout(function () {
 
 
 function load() {
-    driver.get('https://www.instagram.com/myderbent_plus/');
+    var followers = [],
+        id = 0,
+        replay = 5;
 
-    var followers = [];
+    driver.get('https://www.instagram.com/myderbent_plus/').then(function () {
 
-    var handle = function () {
-        var items = driver.findElements(By.css('a[title]'));
+        var handle = function (skip) {
+            if (!skip){
+                driver.executeScript("document.querySelector('body').scrollTop = 200");
+            }
 
-        items.then(function (elements) {
-            var items = elements.map(function (elem) {
-                return elem.getText();
-            });
+            setTimeout(function () {
+                driver.executeScript("document.querySelector('body').scrollTop = 0");
 
-            promise.all(items).then(function (result) {
-                console.log(result)
-            });
+                driver.findElement(By.className('._lm3a0')).then(function(webElement) {
+                    console.log('Loader...')
+                    setTimeout(function () {
+                        handle(skip);
+                    }, 4000);
+                }, function(err) {
 
-        });
+                    var items = driver.findElements(By.css('a[title]'));
+                    items.then(function (elements) {
+                        var items = elements.map(function (elem) {
+                            return elem.getText();
+                        });
 
-        driver.executeScript("document.querySelector('body').scrollTop = 100");
-    };
+                        promise.all(items).then(function (result) {
 
-    setTimeout(function () {
-        driver.findElement(By.xpath('//*[@id="react-root"]/section/main/article/ul/li[2]/a')).click()
+                            for (var key in result){
+                                id++;
+                                followers.push(result[key]);
+                                console.log(id + '. ' + result[key]);
+                            }
+
+                            /* Очищаем список */
+                            driver.executeScript('document.querySelector("ul._ighw9").innerHTML = ""');
+
+                            handle();
+                        });
+                    }, function (err) {
+
+                        if (replay){
+                            replay--;
+
+                            setTimeout(function () {
+                                handle();
+                            }, 10000);
+                        } else {
+
+                            /* Запись данных в файл */
+                            var filename = 'output.txt';
+                            var str = JSON.stringify(followers, null, 4);
+
+                            fs.writeFile(filename, str, function(err){
+                                if(err) {
+                                    console.log(err)
+                                } else {
+                                    console.log('File written!');
+                                }
+                            });
+                        }
+                    });
+
+                });
+            }, 600);
+        };
 
         setTimeout(function () {
-            handle();
+            driver.findElement(By.xpath('//*[@id="react-root"]/section/main/article/ul/li[2]/a')).click()
+
+            setTimeout(function () {
+                driver.executeScript('document.querySelector("ul._ighw9").style.minHeight = "250px"');
+                handle(true);
+            }, timeout);
         }, timeout);
-    }, timeout);
+    });
+}
+
+function isElementPresent(locator) {
+    try {
+        driver.findElement(locator);
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
 
 
-setTimeout(function () {
-
-}, 60000)
+setTimeout(function () {}, 60000)
